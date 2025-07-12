fa9b623dd3d89dcff1f3b45b26958d25edf125ce
Daniel Kostro <kostro.d@gmail.com>
2025-07-12 23:31:44 +0200

    chore: change wasm output dir to be `build`

--- demos/app.js
+++
@@ -1,9 +0,0 @@
-// Those imports are handled by url-loader
-import '../out/wasm/libsvm.wasm';
-
-async function load() {
-  global.SVM = await import('../wasm');
-  import('./bootstrap');
-}
-
-load();
--- wasm.js
+++ wasm.js
@@ -1,5 +1,5 @@
 import load from './src/loadSVM.js';
-import libsvm from './out/wasm/libsvm.js';
+import libsvm from './build/libsvm.js';
 
 export async function loadSVM() {
   const module = await libsvm();
